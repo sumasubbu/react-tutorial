87a0826cb2956d9c76b998b5e7ca9f8b6749cc31
sumasubbu <75.suma@gmail.com>
2019-08-15 12:51:16 +1000

    removed unused imports

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app'
 import './index.css';
-import Table from './table'
 
 ReactDOM.render(<App />, document.getElementById('root'));
